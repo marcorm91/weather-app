e23f40998feff64619170065fc4cde8d696a996d
Marco <maromero@knowmadmod.com>
2023-06-29 13:04:04 +0200

    add: add min and max temperature

--- src/components/weather-current-sunset-temp/WeatherCurrentSunsetTempStyled.js
+++ src/components/weather-current-sunset-temp/WeatherCurrentSunsetTempStyled.js
@@ -3,24 +3,46 @@ import styled from 'styled-components'
 export const WeatherCurrentSunsetTempStyled = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  gap: 21px;
-  > span{
-    font-size: 48px;
-    font-weight: 700;
-    position: relative;
-    &:after{
-      content: 'º';
-      position: absolute;
-      font-size: 26px;
+  flex-direction: column;
+  gap: 4px;
+    > div:first-child{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 21px;
+      > span{
+        font-size: 48px;
+        font-weight: 700;
+        position: relative;
+        &:after{
+          content: 'º';
+          position: absolute;
+          font-size: 26px;
+        }
+      }
+      > div{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        > span{
+          font-size: 12px;
+        }
+      }
     }
-  }
-  > div{
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    > span{
-      font-size: 12px;
+    > div:last-child{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 6px;
+      > span{
+        font-size: 16px;
+        font-weight: 700;
+        position: relative;
+        &:after{
+          content: 'º';
+          position: absolute;
+          font-size: 12px;
+        }
+      }
     }
-  }
 `
