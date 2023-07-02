656db4d87b9ae4066d381be19698208cd7fdedc3
Marco <maromero@knowmadmod.com>
2023-07-02 20:00:00 +0200

    refactor: refactor code and create helpers

--- src/components/weather-current-prediction/WeatherCurrentPrediction.js
+++ src/components/weather-current-prediction/WeatherCurrentPrediction.js
@@ -1,42 +1,38 @@
 import React from 'react'
 import { WeatherCurrentPredictionStyled } from './WeatherCurrentPredictionStyled'
-import { WiSunset, WiSunrise } from "weather-icons-react"
+import { WiSunset, WiSunrise, WiHumidity } from 'weather-icons-react'
+import { findPropertyValueByPeriod, getCurrentHour } from '../../utils/js/helpers'
 
 const WeatherCurrentPrediction = ({ hourlyData, diaryData }) => {
   
   // Getting current date and hour for making later comparisons
-  const currentDate = new Date()
-  const currentHour = currentDate.getHours().toString().padStart(2, '0') 
+  const currentHour = getCurrentHour()
 
   /**
    * HOURLY DATA
    */
 
+  // Get current sunrise and sunset time
   const sunriseTime = hourlyData?.data[0]?.prediccion?.dia[0]?.orto || ''
   const sunsetTime = hourlyData?.data[0]?.prediccion?.dia[0]?.ocaso || ''
-  const temperatureList1 = hourlyData?.data[0]?.prediccion?.dia[0]?.temperatura
-  const temperatureList2 = hourlyData?.data[0]?.prediccion?.dia[1]?.temperatura
-  let currentTemp
 
-  // The data handling is performed to control 2 arrays within the hourlyData object
-  const findTempValueByPeriod = (list, currentHour) => {
-    const foundItem = list.find(item => item.periodo === currentHour);
-    return foundItem ? foundItem.value : null;
-  };
-  currentTemp = findTempValueByPeriod(temperatureList1, currentHour)
-  currentTemp = !currentTemp ? findTempValueByPeriod(temperatureList2, currentHour) : currentTemp;
+  // Get the value closest to the current temperature
+  const temperatureList1 = hourlyData?.data[0]?.prediccion?.dia[0]?.temperatura || []
+  const temperatureList2 = hourlyData?.data[0]?.prediccion?.dia[1]?.temperatura || ''
+  const currentTemp = findPropertyValueByPeriod(temperatureList1, currentHour, ['value']) 
+                      || findPropertyValueByPeriod(temperatureList2, currentHour, ['value'])
 
   // Get the value closest to the current relative humidity from the lists
-  const currentHumidityList1 = hourlyData?.data[0]?.prediccion?.dia[0].humedadRelativa
-  const currentHumidityList2 = hourlyData?.data[0]?.prediccion?.dia[1].humedadRelativa
-  let currentHumidity
+  const currentHumidityList1 = hourlyData?.data[0]?.prediccion?.dia[0]?.humedadRelativa || []
+  const currentHumidityList2 = hourlyData?.data[0]?.prediccion?.dia[1]?.humedadRelativa || []
+  const currentHumidity = findPropertyValueByPeriod(currentHumidityList1, currentHour, ['value']) 
+                          || findPropertyValueByPeriod(currentHumidityList2, currentHour, ['value'])
 
-  const findHumidityValueByPeriod = (list, currentHour) => {
-    const foundItem = list.find(item => item.periodo === currentHour);
-    return foundItem.value
-  };
-  currentHumidity = findHumidityValueByPeriod(currentHumidityList1, currentHour)
-  currentHumidity = !currentHumidity ? findHumidityValueByPeriod(currentHumidityList2, currentHour) : currentHumidity;
+  // Get the value closest to the current wind from the lists
+  const currentWindList1 = hourlyData?.data[0]?.prediccion?.dia[0]?.vientoAndRachaMax || []
+  const currentWindList2 = hourlyData?.data[0]?.prediccion?.dia[1]?.vientoAndRachaMax || []
+  const currentWind = findPropertyValueByPeriod(currentWindList1, currentHour, ['velocidad']) 
+                      || findPropertyValueByPeriod(currentWindList2, currentHour, ['velocidad'])
 
   /**
    * DIARY DATA
@@ -53,7 +49,7 @@ const WeatherCurrentPrediction = ({ hourlyData, diaryData }) => {
           <WiSunrise size={28} color='var(--wa-deep-blue)' />
           <span>{sunriseTime}</span>
         </div>
-        <span>{currentTemp}</span>
+        <span>{currentTemp.value}</span>
         <div>
           <WiSunset size={28} color='var(--wa-deep-blue)' />
           <span>{sunsetTime}</span>
@@ -62,14 +58,13 @@ const WeatherCurrentPrediction = ({ hourlyData, diaryData }) => {
       <div>
         <ul>
           <li className='temperature__wrapper'><span>{temperatureMax}</span>/<span>{temperatureMin}</span></li>
-          <li className='humidity__wrapper'><span>{currentHumidity}</span></li>
-          <li className='wind__wrapper'></li>
+          <li className='humidity__wrapper'><span>{currentHumidity.value}</span><WiHumidity size={28} color='var(--wa-deep-blue)' /></li>
+          <li className='wind__wrapper'><span>{currentWind.velocidad[0]} Km/h</span></li>
         </ul>
-        
       </div>
       
     </WeatherCurrentPredictionStyled>
-  );
+  )
 }
 
 export default WeatherCurrentPrediction
--- src/components/weather-current-sky/WeatherCurrentSky.js
+++ src/components/weather-current-sky/WeatherCurrentSky.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { WeatherCurrentSkyStyled } from './WeatherCurrentSkyStyled'
-import { skyIconMap } from '../../utils/js/skyIcons';
+import { skyIconMap } from '../../utils/js/skyIcons'
+import { findPropertyValueByPeriod, getCurrentHour } from '../../utils/js/helpers'
 
 const WeatherCurrentSky = ({ hourlyData }) => {
 
   // Getting current date and hour for making later comparisons
-  const currentDate = new Date()
-  const currentHour = currentDate.getHours().toString().padStart(2, '0')
+  const currentHour = getCurrentHour()
 
   /**
    * HOURLY DATA
@@ -14,17 +14,10 @@ const WeatherCurrentSky = ({ hourlyData }) => {
 
   const skyList1 = hourlyData?.data[0].prediccion?.dia[0]?.estadoCielo
   const skyList2 = hourlyData?.data[0].prediccion?.dia[1]?.estadoCielo
-  let currentSky
-  
-   // The data handling is performed to control 2 arrays within the hourlyData object
-   const findSkyValueByPeriod = (list, currentHour) => {
-    const foundItem = list.find(item => item.periodo === currentHour);
-    return foundItem ? { value: foundItem.value, descripcion: foundItem.descripcion } : null;
-  };
-  currentSky = findSkyValueByPeriod(skyList1, currentHour)
-  currentSky = !currentSky ? findSkyValueByPeriod(skyList2, currentHour) : currentSky;
+  const currentSky = findPropertyValueByPeriod(skyList1, currentHour, ['value', 'descripcion']) 
+                    || findPropertyValueByPeriod(skyList2, currentHour, ['value', 'descripcion'])
 
-  const { value, descripcion } = currentSky || {}
+  const { value = '', descripcion = '' } = currentSky || {}
   const currentIcon = skyIconMap[value] || null
     
   return (
@@ -32,7 +25,7 @@ const WeatherCurrentSky = ({ hourlyData }) => {
       {currentIcon(128, "var(--wa-deep-blue)")}
       <span>{descripcion}</span>
     </WeatherCurrentSkyStyled>
-  );
+  )
 }
 
 export default WeatherCurrentSky
