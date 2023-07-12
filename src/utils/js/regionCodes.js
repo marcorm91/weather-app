03c82fead08ca5ede473f3fc89997854d8fd8ef9
Marco <maromero@knowmadmod.com>
2023-07-12 09:02:17 +0200

    add: add region codes and obtain random community. Sho info in ad component

---
+++ src/utils/js/regionCodes.js
@@ -0,0 +1,21 @@
+const regionCodes = [
+    { code: 'and', community: 'Andalucía' },
+    { code: 'arn', community: 'Aragón' },
+    { code: 'ast', community: 'Asturias' },
+    { code: 'bal', community: 'Ballears, Illes' },
+    { code: 'coo', community: 'Canarias' },
+    { code: 'can', community: 'Cantabria' },
+    { code: 'cle', community: 'Castilla y León' },
+    { code: 'clm', community: 'Castilla - La Mancha' },
+    { code: 'cat', community: 'Cataluña' },
+    { code: 'val', community: 'Comunitat Valenciana' },
+    { code: 'ext', community: 'Extremadura' },
+    { code: 'gal', community: 'Galicia' },
+    { code: 'mad', community: 'Madrid, Comunidad de' },
+    { code: 'mur', community: 'Murcia, Región de' },
+    { code: 'nav', community: 'Navarra, Comunidad Foral de' },
+    { code: 'pva', community: 'País Vasco' },
+    { code: 'rio', community: 'Rioja, La' }
+  ]
+  
+  export default regionCodes  
