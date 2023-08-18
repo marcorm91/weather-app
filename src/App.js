122115c56a12023853d85cf74484882557aeedaf
Marco <marco_antonio88_9@hotmail.com>
2023-08-18 13:15:34 +0200

    update: replace browserrouter by hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import WeatherHeader from './components/weather-header/WeatherHeader'
 import WeatherFooter from './components/weather-footer/WeatherFooter'
 import WeatherHome from './pages/weather-home/WeatherHome'
